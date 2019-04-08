9e4a1b96051fe95c634b6540e781c879043c4377
Varad Khandelwal <simplyvarad@gmail.com>
2019-04-08 15:39:57 -0500

    Update startups.js

--- routes/startups.js
+++ routes/startups.js
@@ -1,6 +1,8 @@
 // var secrets = require('../config/secrets');
-var sql = require('../connection.js');
-var bodyParser = require('body-parser')
+var connect = require('../connection.js');
+var sql = connect.connection;
+var handle = connect.handleDisconnect;
+// var bodyParser = require('body-parser')
 module.exports = function (router) {
 
     var startupsRoute = router.route('/');
@@ -8,47 +10,63 @@ module.exports = function (router) {
 
     startupsRoute.get(function (req, res) {
         // var connectionString = secrets.token;
-        console.log("reached get")
+        // console.log("reached get")
         var query;
         if (req.query.name) { query = "SELECT * FROM startup WHERE Name LIKE '%" + JSON.parse(req.query.name) + "%';"; }
         else { query = "SELECT * FROM startup"; }
-        sql.connect( function(err) {
-                if(err) { console.log(err); }
-        });
+        // sql.connect( function(err) {
+        //         if(err) { console.log(err); }
+        // });
+        handle(sql);
         sql.query({sql: query}, function(err, rows, fields) {
             if (err) { throw err; }
             // console.log(JSON.parse(req.query.name));
             res.send(rows);
         })
-        sql.end();
+        // sql.end();
     });
 
     startupsRoute.post(function (req, res) {
+        // sql.connect( function(err) {
+        //     if(err) { console.log(err); }
+        // });
+        handle(sql);
         sql.query({sql: "INSERT INTO startup SET ?", values: [req.body]}, function(err, rows) {
             if (err) { throw err; }
             res.send(rows)
         })
+        // sql.end();
     });
 
     startupRoute.get(function (req, res) {
-        // console.log(req.params.id)
+        console.log(req.params.id);
+        // sql.connect( function(err) {
+        //     if(err) { console.log(err); }
+        // });
+        handle(sql);
         sql.query({sql: "SELECT * FROM startup WHERE StartupID = ?", values: [JSON.parse(req.params.id)]}, function(err, rows) {
             if (err) { throw err; }
             res.send(rows);
         })
+        // sql.end();
     });
 
     startupRoute.put(function (req, res) {
         console.log(req.body);
-
+        // sql.connect( function(err) {
+        //     if(err) { console.log(err); }
+        // });
+        handle(sql)
         sql.query({sql: "UPDATE startup SET ? WHERE StartupID = ?", values: [req.body, JSON.parse(req.params.id)]}, function(err, rows) {
            if (err) {throw err; }
             res.send(rows);
         })
+        // sql.end();
         //"UPDATE Startup SET ? WHERE StartupID = ?" values: [req.body, JSON.parse(req.params.id)]
     });
 
     startupRoute.delete(function (req, res) {
+        handle(sql);
         sql.query({sql: "DELETE FROM startup WHERE StartupID = ?", values: [JSON.parse(req.params.id)]}, function(err, rows) {
             if (err) { throw err; }
             res.send(rows);
